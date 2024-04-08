0e6b6770c479f103655c9c8d70bc7eee88368abb
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-08 11:25:32 +0530

    Update auth.js
    Co-Authored-By: SupunNarangoda <113084899+supunnarangoda@users.noreply.github.com>

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,16 @@
-import { Router } from 'express';
-import  Auth  from '../models/auth.js'
+import { Router } from "express";
+import Auth from "../models/auth.js";
 const authRouter = Router();
 
 authRouter.post("/signup", (req, res) => {
-    const { email, password, name } = req.headers;
-    const result = new Auth(email, password).createUser()
+  const { email, password, name } = req.headers;
+  const result = new Auth(email, password).createUser();
 });
 
 authRouter.post("/login", (req, res) => {
-    const { email, password } = req.headers;
-    const result = new Auth(email, password).loginUser()
+  const { email, password } = req.headers;
+  console.log(email, password);
+  const result = new Auth(email, password).loginUser();
 });
 
 export default authRouter;
