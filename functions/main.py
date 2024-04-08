da33257c3f5513c1095596c124270d4d3e03d80a
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-08 10:35:02 +0530

    debug3
    Co-Authored-By: SupunNarangoda <113084899+supunnarangoda@users.noreply.github.com>

--- functions/main.py
+++ functions/main.py
@@ -1,13 +1,22 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
+"""
+Module docstring: This module contains a Cloud Function for Firebase that responds to HTTP requests.
+"""
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+initialize_app()
+
+
+@https_fn.on_request()
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    """
+    This function is an example of a Cloud Function for Firebase that responds to HTTP requests.
+    It returns a simple "Hello World!" response for any incoming request.
+
+    Args:
+        req (https_fn.Request): The request object.
+
+    Returns:
+        https_fn.Response: A response object with a status code and optional body.
+    """
+    return https_fn.Response(f"Hello world! {req}")
