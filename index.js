a3c8def3f8775945ae8c3ae90d0d3bc17c473a6a
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-08 13:15:43 +0530

    6 implement user login verification process addon
    Co-Authored-By: SupunNarangoda <113084899+supunnarangoda@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import PORT from './config/app.js';
-import authRouter from './routes/auth.js'
+import express from "express";
+import PORT from "./config/app.js";
+import authRouter from "./routes/auth.js";
 // import dotenv from 'dotenv'
 
 // dotenv.config()
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
 
-app.use('/api/auth/', authRouter);
+app.use("/api/auth/", authRouter);
