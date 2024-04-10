5fb3146cca83928d5543f71305eb81ee2e03b73c
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-10 21:20:03 +0530

    Update auth.js
    Co-Authored-By: SupunNarangoda <113084899+supunnarangoda@users.noreply.github.com>

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import authInitiateObjects from "../middlewares/auth/authInitiateObjects.js";
-import extractUidAndVerification from "../middlewares/extractUidAndVerification.js";
 
 const authRouter = Router();
 
@@ -9,14 +8,9 @@ authRouter.post("/signup", authInitiateObjects, async (req, res) => {
   res.json({ status: response ? 200 : 500, uid: uid });
 });
 
-authRouter.post(
-  "/login",
-  extractUidAndVerification,
-  authInitiateObjects,
-  (req, res) => {
-    const [response, uid] = req.auth.loginUser();
-    res.json({ status: response ? 200 : 500, uid: uid });
-  }
-);
+authRouter.post("/login", authInitiateObjects, (req, res) => {
+  const [response, uid] = req.auth.loginUser();
+  res.json({ status: response ? 200 : 500, uid: uid });
+});
 
 export default authRouter;
