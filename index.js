de273d8a131b8011b1dc2157cc3552c66290e0b6
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-12 14:38:53 +0530

    Add News Articles Section
    This commit introduces a new section dedicated to news articles on our website. The purpose of this addition is to keep our audience informed about the latest developments, industry trends, and relevant updates. By incorporating a news section, we aim to enhance user engagement and provide valuable content for our visitors. Further discussions on the implementation details and content strategy for this new section will follow.
    
    Co-Authored-By: SupunNarangoda <113084899+supunnarangoda@users.noreply.github.com>

--- index.js
+++ index.js
@@ -64,6 +64,26 @@
 import test from "config/firebase.js";
 
 import express from "express";
+<<<<<<< HEAD
+=======
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import PORT from "./config/app.js";
+import authRouter from "./routes/auth.js";
+import userDetailsRouter from "./routes/userDetails.js";
+import msgRouter from "./routes/chat/message.js";
+import grpRouter from "./routes/chat/group.js";
+import commRouter from "./routes/community/community.js";
+import qpRouter from "./routes/community/quick_projects/quick_project.js";
+import iRouter from "./routes/community/initiatives/initiative.js";
+import commentRouter from "./routes/comment.js";
+import projectRouter from "./routes/projects/project.js";
+import postRouter from "./routes/organizations/posts.js";
+import checkBanStatus from "./middlewares/admin/checkBanStatus.js";
+import banRouter from "./routes/admin/ban.js";
+import newsRouter from "./routes/admin/news.js";
+
+>>>>>>> 8f857ff (Add News Articles Section)
 const app = express();
 const firebase = require("./services/firebase.js");
 
@@ -71,7 +91,42 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
+<<<<<<< HEAD
 app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
   firebase.init();
 });
+=======
+// Use routers
+app.use(checkBanStatus);
+app.use("/api/auth", authRouter);
+app.use("/api/usr", userDetailsRouter);
+app.use("/api/chat/msg", msgRouter);
+app.use("/api/chat/grp", grpRouter);
+app.use("/api/community", commRouter);
+app.use("/api/events/quickproject", qpRouter);
+app.use("/api/events/initiative", iRouter);
+app.use("/api/comments/", commentRouter);
+app.use("/api/prj", projectRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/ban", banRouter);
+app.use("/api/news", newsRouter);
+/**
+ * Error handling middleware.
+ * @param {Error} err - The error object.
+ * @param {IncomingMessage} req - The HTTP request object.
+ * @param {ServerResponse} res - The HTTP response object.
+ * @param {any} next - The next middleware function in the stack.
+ */
+app.use((err, _, res, __) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// Start the server
+const server = app.listen(PORT, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`App listening at http://${host}:${port}`);
+});
+>>>>>>> 8f857ff (Add News Articles Section)
