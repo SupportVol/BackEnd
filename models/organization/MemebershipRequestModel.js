aafe63859731d9285395407e9ab8aa399782d1cf
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-16 14:46:03 +0530

    Redefine Organization Structure & Add Features
    Refactored organization structure to enhance efficiency and user experience. Added new features to enrich the volunteer management system, improving capabilities and usability. This commit addresses issue #47.

---
+++ models/organization/MemebershipRequestModel.js
@@ -0,0 +1,75 @@
+import { Authentication } from "../../firebaseCP/authentication";
+import Firestore from "../../firebaseCP/firestore";
+import FirestoreAbstract from "../../utils/firestore/FirestoreAbstract";
+import updateData from "../../utils/firestore/updateData";
+
+export default class MembershipRequestModel extends FirestoreAbstract {
+  constructor(
+    registrationCertificateUrl,
+    annualReportUrl,
+    legalDocumentsUrl,
+    name,
+    email,
+    password,
+    description
+  ) {
+    this.createStructure = {
+      name: name,
+      description: description,
+      email: email,
+      password: password,
+      registrationCertificateUrl: registrationCertificateUrl,
+      annualReportUrl: annualReportUrl,
+      legalDocumentsUrl: legalDocumentsUrl,
+      creationDate: Date.now(),
+    };
+    const currentRecord = this.read();
+    this.updateStructure = updateData(
+      [
+        "name",
+        "description",
+        "email",
+        "password",
+        "registrationCertificateUrl",
+        "annualReportUrl",
+        "legalDocumentsUrl",
+        "creationDate",
+        "accepted",
+      ],
+      [
+        name,
+        description,
+        email,
+        password,
+        registrationCertificateUrl,
+        annualReportUrl,
+        legalDocumentsUrl,
+        Date.now(),
+        false,
+      ],
+      currentRecord
+    );
+    this.fs = new Firestore("organizationsRequests", false);
+  }
+
+  approve() {
+    const record = this.fs.read();
+    this.fs.delete();
+    this.fs = new Firestore("organizations", false);
+    const orgID = this.fs.create(record)[1];
+    this.auth = new Authentication();
+    this.auth.createUser({ email: record.email, password: record.password });
+    this.authFS = new Firestore("users", orgID);
+    this.authFS.create({
+      orgID: orgID,
+      role: "Organization",
+      level: 0,
+    });
+    return orgID;
+  }
+
+  decline() {
+    this.fs.delete();
+    return true;
+  }
+}
